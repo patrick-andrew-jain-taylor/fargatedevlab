257cd755b508028708b8f40b74ca83a011cba354
Patrick Jain-Taylor <pjaintaylor@eatonvance.com>
2020-05-13 16:49:57 -0400

    Adding instance profile for C9 instance.

---
+++ c9_instance_profile.tf
@@ -0,0 +1,25 @@
+data "aws_iam_policy_document" "this" {
+  version = "2012-10-17"
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type        = "Service"
+    }
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "this" {
+  assume_role_policy = data.aws_iam_policy_document.this.json
+  name               = "fargatedevlab-admin"
+}
+
+resource "aws_iam_instance_profile" "this" {
+  name = "fargatedevlab-admin-profile"
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.this.name
+}
